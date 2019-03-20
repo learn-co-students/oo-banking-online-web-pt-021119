8f6e138a617bf8aacb1ba451932b0731170bf4f0
Henry Sztanski <henry@sztanski.com>
2019-03-20 22:17:46 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,21 @@ class Transfer
   end
 
   def execute_transaction
-    if !self.sender.valid?
+    if !self.sender.valid? || sender.balance < self.amount
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     elsif self.status == "pending"
       self.sender.balance -= amount
       self.receiver.balance += amount
       self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.receiver.balance -= amount
+      self.sender.balance += amount
+      self.status = "reversed"
     end
   end
 end
