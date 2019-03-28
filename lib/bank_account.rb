86809c4584149dd95b96fd8d9593f18d4377f38e
Malcolm Leake <echelon1089@gmail.com>
2019-03-28 06:00:30 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
 class BankAccount
 
+attr_reader :balance, :status, :name
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit(money)
+@balance = 1000 + money
+#binding.pry
+end
+
+def display_balance
+   "Your balance is $#{@balance}."
+end
+
+
 end
